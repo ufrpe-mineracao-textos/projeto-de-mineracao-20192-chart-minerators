6b0ab5e753934c4cfbe860cec785b0a5fe3db534
Giuseppe Fiorentino Neto <fiorentinogiuseppebcc@gmail.com>
2019-11-03 11:39:42 -0300

    Update scriptMenor.sh

--- base/scriptMenor.sh
+++ base/scriptMenor.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ls /content/model-* | sed -r 's/.*-loss([0-9]*\.[0-9]*)-.*/\1/g' | sort | head -1
+menor=`ls /content/model-* | sed -r 's/.*-loss([0-9]*\.[0-9]*)-.*/\1/g' | sort | head -1`
+ls *"$menor"*
